99a06a39fc2f626b8839ad61e6bfb52accc5f39f
shreyashbhise16 <shreyashbhise16@gmail.com>
2023-09-24 17:39:43 -0400

    file is added

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "eksterrabucket"
+    bucket = "cloudops-terraform-demo4"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
-    dynamodb_table = "dynamoDB-terra"
+    dynamodb_table = "dynamoDB-terra-demo4"
   }
 }
